09fb84a5f079f78feac25f1e696da3b48dd1edbc
Charles Jayson Dadios <cjldadios.uplb@gmail.com>
2021-03-25 12:15:38 +0800

    first add

---
+++ index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5002;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, function(error) { console.log(error);
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase! For real.");
+});
+
+exports.atlas = functions.https.onRequest(app);
+
+
